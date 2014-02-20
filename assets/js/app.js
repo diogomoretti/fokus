6070b58e4157117ee0581a711aba3d57511cdd37
Diogo Moretti <diogo.pearljam@gmail.com>
2014-02-20 18:00:15 -0300

    Some tests

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,70 @@
-/*
+
+
 var navigatorWidth = $(window).width();
+var navigatorHeight = $(window).height();
 
 var element = document.getElementById("element");
 
 var rect = element.getBoundingClientRect();
 
-console.log(navigatorWidth)
+console.log($('#element')[0])
 console.log(rect.top, rect.right, rect.bottom, rect.left);
 
-var fokusTop = rect.top - 5;
-var fokusLeft = rect.left - 5;
-var fokusRight = rect.right + 5;
+var distanceTop = rect.top - 5;
+var distanceLeft = rect.left - 5;
+var distanceRight = rect.right + 5;
+var distanceBottom = rect.bottom + 5;
 
-var r = navigatorWidth - fokusRight;
+var r = navigatorWidth - distanceRight;
+
+var c = navigatorWidth - distanceLeft;
+
+var d = navigatorWidth - (r + distanceLeft);
+
+var v = navigatorHeight - distanceBottom;
+
+$(document).keydown(function(event) {
+    var ch = event.which;
+    if (ch == 27) {
+        console.log('Apertei ESC');
+    }
+});
+
+console.log('Altura do Navegador: ' + navigatorHeight)
+console.log('Distancia do Bottom: ' + distanceBottom)
 
 var elem = $('body')
-elem.prepend('<div class="fokus fokus-top"/><div class="fokus fokus-bottom"/><div class="fokus fokus-right"/><div class="fokus fokus-left"/>')
+elem.prepend('<div class="fokus fokus-top"/><div class="fokus fokus-right"/><div class="fokus fokus-bottom"/><div class="fokus fokus-left"/><div class="fokus-element"/>')
 elem.find('.fokus-top').css({
-                        width  : '100%',
-                        height : fokusTop + 'px'
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        width : '100%',
+                        height : distanceTop + 'px'
                     });
 elem.find('.fokus-right').css({
-                        width  : r + 'px',
-                        top : fokusTop + 'px',
-                        left : fokusRight + 'px'
+                        right: 0,
+                        bottom: 0,
+                        width : r + 'px',
+                        top : distanceTop + 'px',
+                        left : distanceRight + 'px'
                     });
 elem.find('.fokus-bottom').css({
-                        width  : fokusLeft + 'px',
-                        top : fokusTop + 'px'
+                        bottom: 0,
+                        left : distanceLeft + 'px',
+                        right : distanceRight + 'px',
+                        width : d + 'px',
+                        height : v + 'px'
                     });
 elem.find('.fokus-left').css({
-                        width  : fokusLeft + 'px',
-                        top : fokusTop + 'px'
+                        left: 0,
+                        bottom: 0,
+                        width : distanceLeft + 'px',
+                        top : distanceTop + 'px'
                     });
+
+$('.fokus').on('click', function () {
+
+    //console.log('Clicou fora!')
+    $('.fokus').remove();
+});
